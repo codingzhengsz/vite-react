90757d377f928cfdb743f9ba8bef21f8dd27d1d3
zhengsz <suttonprog@gmail.com>
2024-01-23 23:13:27 +0800

    update dependency

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,9 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import eslint from "vite-plugin-eslint";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-	plugins: [react(), eslint()],
+	plugins: [react()],
 	resolve: {
 		alias: [
 			{ find: "@/Assets", replacement: "/src/Assets" },
